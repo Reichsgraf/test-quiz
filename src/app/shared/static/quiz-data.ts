e5075a0c7d09f065639cf4fcb8af20389d4b3d0e
@nikita <angdev329@gmail.com>
2023-05-18 12:31:32 +0300

    quiz data was moved in static

---
+++ src/app/shared/static/quiz-data.ts
@@ -0,0 +1,27 @@
+export const quizData = [
+  {
+    key: 'gender',
+    value: 'Select your baby’s gender',
+    variants: ['BOY', 'GIRL']
+  },
+  {
+    key: 'age',
+    value: 'How old is your baby?',
+    variants: ['Under 6 months', '6-8 months', '8-10 months', 'Over 10 months']
+  },
+  {
+    key: 'weight',
+    value: 'What is your baby\'s current weight?',
+    variants: ['Under 10 pounds', '10-15 pounds', '15-20 pounds', 'Over 20 pounds']
+  },
+  {
+    key: 'health',
+    value: 'Your baby\'s overall health and development',
+    variants: ['Healthy and thriving', 'Some health concerns', 'Developmental delays or concerns']
+  },
+  {
+    key: 'feeding',
+    value: 'Your baby\'s current feeding routine',
+    variants: ['Exclusively breastfed', 'Exclusively formula-fed', 'A combination of breast milk and formula']
+  },
+];
